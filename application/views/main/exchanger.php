5edafadbe982e8bbdfe887154a429487ac28f3ab
wayan parwita <wyparwita@gmail.com>
2016-01-26 22:58:56 +0800

    update tampilan exchanger

--- application/views/main/exchanger.php
+++ application/views/main/exchanger.php
@@ -1,3 +1,22 @@
+<style>
+	.table>tbody>tr>td {
+		vertical-align: middle;
+		text-align:center;
+	}
+	.par_exc{
+		padding:15px 10px;
+		background: #fff; /* For browsers that do not support gradients */
+		background: -webkit-linear-gradient(#CCCCCC,#ffffff); /* For Safari 5.1 to 6.0 */
+		background: -o-linear-gradient(#CCCCCC,#ffffff); /* For Opera 11.1 to 12.0 */
+		background: -moz-linear-gradient(#CCCCCC,#ffffff); /* For Firefox 3.6 to 15 */
+		background: linear-gradient(#CCCCCC,#ffffff); /* Standard syntax */	
+		font-size:15px;
+		font-weight:bold;
+		-webkit-box-shadow: 0px 4px 14px -2px rgba(0,0,0,0.81);
+		-moz-box-shadow: 0px 4px 14px -2px rgba(0,0,0,0.81);
+		box-shadow: 0px 4px 14px -2px rgba(0,0,0,0.81);
+	}
+</style>
 <div class="margin_top5"></div> 
 <div class="container">
 <?php isset($side_menu)?$this->load->view($side_menu):'side_menu'?>
@@ -19,7 +38,7 @@
                     <p>Dengan Bantuan Customer Support khusus yang selalu siap melayani anda, transaksi anda akan di proses secara cepat.</p>
                     
                     <br><br>
-                    <div class="col-xs-12" style="background-color:#ddd; padding:15px">
+                    <div class="col-xs-12" style="background-color:#F1F1F1; padding:15px">
                     	<h4 style="text-align:center; font-weight:bold">Kurs Jual Beli FGS</h4>
                         <p style="text-align:center">Stok tidak mengikat.<br>Klik kurs BELI untuk menukarkan Rupiah anda dengan E-Currency<br>Klik kurs JUAL untuk menukarkan E-Currency anda dengan rupiah</p>
                         <div class="tablewrap table-responsive">
@@ -32,33 +51,39 @@
                                 </thead>
                                 <tbody style="margin-top:20px">
                                 	<tr>
-                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/unnamed.png" alt="" class="img_left11" /></td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
+                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/exness.png" alt="" /></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
                                     </tr>
                                     <tr>
-                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/unnamed.png" alt="" class="img_left11" /></td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
+                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/instaforex.png" alt="" /></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
                                     </tr>
                                     <tr>
-                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/unnamed.png" alt="" class="img_left11" /></td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
-                                        <td>Rp 13.700</td>
+                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/unnamed.png" alt="" /></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                    </tr>
+                                    <tr>
+                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/orbex.png" alt="" /></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                    </tr>
+                                    <tr>
+                                    	<td><img src="<?=base_url()?>/media/images/logo_broker/HotForex-logo.png" alt="" /></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
+                                        <td><div class="par_exc">Rp 13.700</div></td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                     </div>
-                    
-                    
-                    <ul>
-                        <li><em>** Dibawah ini contoh tampilan rate harga Jual Beli yang nanti ditampilkan di web ya bro. Harga Jual Beli di set admin di admin area.</em></li>
-                    </ul>
-					<img class="img-responsive" style="text-align:center" src="<?=base_url()?>/media/images/deposit5.jpg">
                     <br><br>
                     <h4><strong>Waktu Layanan</strong></h4>
                     <table border="0" cellpadding="0" cellspacing="0" style="width:214px">
